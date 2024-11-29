77ca6ec478e73c75bfc1cf4245b9a0aee3dc4cd2
xexx1 <fam8252835@gmail.com>
2024-11-29 12:55:38 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+// get port number from enviroment settings
+require('dotenv').config({ path: './config.env'});
+const port = process.env.PORT || 3000;
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const productRoute = require('./routes/product.route');
+
+//  cross origin resource sharing
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// path url 
+// app.[method]();
+
+// match GET localhost:4000/
+app.get("/", (req,res) =>{
+    res.send("Sawasdee");
+});
+
+app.use("/products", productRoute);
+
+app.listen(port, () => {
+  console.log("App started at port: " + port);
+});
